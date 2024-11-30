c1df6998d030fe0f8271c61bc332794faf6d6c9b
chinhdev <chinhtdph40493@fpt.edu.vn>
2024-11-30 21:57:29 +0700

    add HomeController, login, register,main, file assests, update SecurityConfig

---
+++ be/src/main/resources/static/assets/js/userjs.js
@@ -0,0 +1,18 @@
+$(document).on('ready', function () {
+    if (window.localStorage.getItem('hs-builder-popover') === null) {
+        $('#builderPopover').popover('show')
+            .on('shown.bs.popover', function () {
+                $('.popover').last().addClass('popover-dark')
+            });
+
+        $(document).on('click', '#closeBuilderPopover', function () {
+            window.localStorage.setItem('hs-builder-popover', true);
+            $('#builderPopover').popover('dispose');
+        });
+    } else {
+        $('#builderPopover').on('show.bs.popover', function () {
+            return false
+        });
+    }
+    
+});
