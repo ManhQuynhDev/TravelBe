39bd8d9875170af5fe8fe3e542b19444c169251a
quynhlm.dev <quynhlm.dev@gmail.com>
2025-01-17 01:35:59 +0700

    sort date travel_plan

--- be/src/main/java/com/quynhlm/dev/be/repositories/MemberPlanRepository.java
+++ be/src/main/java/com/quynhlm/dev/be/repositories/MemberPlanRepository.java
@@ -13,28 +13,28 @@ import com.quynhlm.dev.be.model.entity.MemberPlan;
 
 public interface MemberPlanRepository extends JpaRepository<MemberPlan, Integer> {
 
-    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN TRUE ELSE FALSE END FROM MemberPlan m WHERE m.userId = :userId AND m.planId = :planId AND delflag = 0")
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN TRUE ELSE FALSE END FROM MemberPlan m WHERE m.userId = :userId AND m.planId = :planId AND m.delflag = 0")
     boolean existsByUserIdAndPlanId(@Param("userId") Integer userId, @Param("planId") Integer planId);
 
-    @Query("SELECT m FROM MemberPlan m WHERE m.userId = :userId and m.planId = :planId AND delflag = 0")
+    @Query("SELECT m FROM MemberPlan m WHERE m.userId = :userId and m.planId = :planId AND m.delflag = 0")
     MemberPlan findMemberById(
             @Param("userId") Integer userId, @Param("planId") Integer planId);
 
-    @Query("SELECT m FROM MemberPlan m WHERE m.userId = :userId AND delflag = 0")
+    @Query("SELECT m FROM MemberPlan m WHERE m.userId = :userId AND m.delflag = 0")
     List<MemberPlan> findMemberByUserId(
             @Param("userId") Integer userId);
 
-    @Query("SELECT m FROM MemberPlan m WHERE m.planId = :planId AND delflag = 0")
+    @Query("SELECT m FROM MemberPlan m WHERE m.planId = :planId AND m.delflag = 0")
     List<MemberPlan> findMemberByPlanId(
             @Param("planId") Integer planId);
 
-    @Query("SELECT m FROM MemberPlan m WHERE m.userId = :userId AND m.planId = :planId AND m.status IN :status AND delflag = 0")
+    @Query("SELECT m FROM MemberPlan m WHERE m.userId = :userId AND m.planId = :planId AND m.status IN :status AND m.delflag = 0")
     Optional<MemberPlan> findByUserIdAndPlanIdAndStatusIn(
             @Param("userId") Integer userId,
             @Param("planId") Integer planId,
             @Param("status") List<String> status);
 
-    @Query("SELECT m FROM MemberPlan m WHERE m.planId = :planId AND m.role <> 'ADMIN' AND delflag = 0")
+    @Query("SELECT m FROM MemberPlan m WHERE m.planId = :planId AND m.role <> 'ADMIN' AND m.delflag = 0")
     Page<MemberPlan> getRequestToJoinPlan(
             @Param("planId") Integer planId,
             Pageable pageable);
@@ -52,7 +52,7 @@ public interface MemberPlanRepository extends JpaRepository<MemberPlan, Integer>
             inner join travel_plan t on t.id = m.plan_id
             inner join user u on u.id = m.user_id
             where m.plan_id  = :planId
-            AND delflag = 0;
+            AND m.delflag = 0;
                                     """, nativeQuery = true)
     Page<Object[]> foundMemberJoinWithPlan(@Param("planId") Integer planId, Pageable pageable);
 
@@ -68,7 +68,7 @@ public interface MemberPlanRepository extends JpaRepository<MemberPlan, Integer>
              from member_plan m
             inner join travel_plan t on t.id = m.plan_id
             inner join user u on u.id = m.user_id
-            where m.plan_id =:planId AND delflag = 0;
+            where m.plan_id =:planId AND m.delflag = 0;
                                     """, nativeQuery = true)
     List<Object[]> foundMemberJoinPlan(@Param("planId") Integer planId);
 }
